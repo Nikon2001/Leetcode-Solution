83842927d9c0dfb66c49bf88f6772359ee483933
Nikon2001 <nikonmeher@gmail.com>
2024-02-21 09:45:33 +0530

    Add new solution

---
+++ 0061. Rotate List/0061.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head:
+            return head
+        
+        #connect tail to head
+        curr = head
+        length = 1
+        while curr.next:
+            curr = curr.next
+            length = length+1
+        curr.next = head
+
+        #move to new head
+        k = length - (k%length)
+        while k>0:
+            curr = curr.next
+            k = k-1
+
+        #disconnect and return new head
+        newhead = curr.next
+        curr.next = None
+        return newhead
